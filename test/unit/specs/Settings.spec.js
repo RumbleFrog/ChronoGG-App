f75056ba8d737ef0c76c2fa8004d8837c1a76238
RumbleFrog <xxlosttechxx@gmail.com>
2018-03-19 23:00:47 -0400

    :white_check_mark: Settings Unit

---
+++ test/unit/specs/Settings.spec.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import Settings from '@/components/Settings';
+
+describe('Settings.vue', () => {
+  it('should render correct contents', () => {
+    const vm = new Vue({
+      el: document.createElement('div'),
+      render: h => h(Settings),
+    }).$mount();
+
+    expect(vm.$el.querySelector('button.chrono-button span').textContent).to.contain('Save Settings');
+  });
+});
