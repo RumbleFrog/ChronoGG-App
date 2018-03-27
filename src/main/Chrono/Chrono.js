d7407a4be9188146bbf9b146996585e1cb4ea968
RumbleFrog <xxlosttechxx@gmail.com>
2018-03-27 19:37:31 -0400

    :bulb: Chrono Module

--- src/main/Chrono/Chrono.js
+++ src/main/Chrono/Chrono.js
@@ -4,10 +4,18 @@ import Request from "request";
 
 const Chrono = {};
 
+/**
+ * Creates a base request func w/ ChronoGG Api Endpoint
+ */
 Chrono.ChronoQuest = Request.defaults({
   baseUrl: "https://api.chrono.gg/"
 });
 
+/**
+ * Gets current sale from `/sale` endpoint from baseUrl
+ *
+ * @returns {Promise<any>} Resolves w/o parsed JSON object, otherwise rejects with request error
+ */
 Chrono.getSale = function() {
   return new Promise((resolve, reject) => {
     Chrono.ChronoQuest("sale", (err, res, body) => {
@@ -17,6 +25,11 @@ Chrono.getSale = function() {
   });
 };
 
+/**
+ * Gets shop listing from `/shop` endpoint from baseUrl
+ *
+ * @returns {Promise<any>} Resolves w/ parsed JSON object, otherwise rejects with request error
+ */
 Chrono.getShop = function() {
   return new Promise((resolve, reject) => {
     Chrono.ChronoQuest("shop", (err, res, body) => {
